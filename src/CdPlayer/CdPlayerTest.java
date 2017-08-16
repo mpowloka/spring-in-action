c32a06ff4a22502228c7156d6518a2e003b9dafb
michal_powloka <michal.powloka@accenture.com>
2017-08-16 17:02:13 +0200

    Created first package with java based config. It tests autowiring annotations.

---
+++ src/CdPlayer/CdPlayerTest.java
@@ -0,0 +1,22 @@
+package CdPlayer;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = CdPlayerConfig.class)
+public class CdPlayerTest {
+
+    @Autowired
+    private CompactDisc compactDisc;
+
+    @Test
+    public void test() {
+        Assert.assertNotNull(compactDisc);
+    }
+}
---
+++ src/CdPlayer/CompactDisc.java
@@ -0,0 +1,5 @@
+package CdPlayer;
+
+public interface CompactDisc {
+    void play();
+}
